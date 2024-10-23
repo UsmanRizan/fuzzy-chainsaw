a2f9188e7d104b6537a913e3954e4ec65fc5c475
UsmanRizan <usmanrizan6@gmail.com>
2024-10-23 17:06:27 +0530

    connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express()
 
+console.log(process.env.MONGO_URI)
+
+app.get("/products",(req,res)=>{
+    res.send("Hello")
+})
+
 app.listen(5000,()=>{
-    console.log("server ")
+    connectDB();
+    console.log("server is ready at https://localhost:5000 ")
 })
