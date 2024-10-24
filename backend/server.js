8e383d44965d5026dca7d5ad6dcf6260d8a059b2
UsmanRizan <usmanrizan6@gmail.com>
2024-10-24 15:16:19 +0530

    Created api for creating products

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js';
+import Product from '../models/products.model.js';
 
 dotenv.config();
 
@@ -8,8 +9,21 @@ const app = express()
 
 console.log(process.env.MONGO_URI)
 
-app.get("/products",(req,res)=>{
-    res.send("Hello")
+app.post("/products",async (req,res)=>{
+    const product = req.body
+    if (!product.name||!product.price||!product.image) {
+        return res.status(400).json({success:false,message:"please provide all fields"})
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+    } catch (error) {
+        console.error("Error in creating product:",error.message)
+        res.status(500).json({success:false,message:"server error"})
+    }
+
 })
 
 app.listen(5000,()=>{
