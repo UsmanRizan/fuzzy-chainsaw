c3c0c0b3a82b59adaeb2e611cd9ebd8789a85661
UsmanRizan <usmanrizan6@gmail.com>
2024-10-28 20:40:41 +0530

    Updated api for delete product

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,15 @@ app.post("/api/products",async (req,res)=>{
 
 app.delete("/api/products/:id",async(req,res)=>{
     const {id} = req.params
-    console.log(":id",id)
+
+    try {
+        await Product.findByIdAndDelete(id)
+        res.status(200).json({success:true,message:"product deleted"})
+    } catch (error) {
+        console.error("Error in deleting product:",error.message)
+        res.status(500).json({success:false,message:"server error"})
+    }
+
 })
 
 app.listen(5000,()=>{
