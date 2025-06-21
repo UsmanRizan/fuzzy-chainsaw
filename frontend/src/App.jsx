b544b568cfb018dcf24bd18bd51544adcbe85b57
UsmanRizan <usmanrizan6@gmail.com>
2025-06-21 21:08:06 +0530

    Nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Box minH={"100vh"}></Box>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/create" element={<CreatePage />}></Route>
+      </Routes>
     </>
   );
 }
